69630d36ddb5e0218e7dd672cec8ea38fa3d8e08
matt <matt@matthewmason.ca>
2016-12-10 04:22:17 -0400

    All the current Scene

---
+++ AirshipDeathraceNineThousand/Assets/Scripts/PeopleActionDecider.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeopleActionDecider : MonoBehaviour
+{
+    public enum Task
+    {
+        Idle,
+        Steer,
+        Throttle,
+        Stoke,
+        Hook
+    }
+
+    [Range(0, 1)]
+    [SerializeField]
+    private float throttleIdeal = 0.5f;
+    [Range(0, 1)]
+    [SerializeField]
+    private float throttleCurrent = 0.5f;
+
+    [Range(-45, 45)]
+    [SerializeField]
+    private float steerAngleIdeal = 0.0f;
+    [Range(-45, 45)]
+    [SerializeField]
+    private float steerAngleCurrent = 0.0f;
+
+    [SerializeField]
+    private bool hooking;
+    [SerializeField]
+    private bool stoking;
+
+    private List<Task> TaskList;
+
+
+
+    public void ThrottleUp()
+    {
+        if (throttleIdeal <= 0.95f)
+            throttleIdeal += 0.05f;
+
+        if (TaskList.Contains(Task.Throttle))
+            TaskList.Add(Task.Throttle);
+    }
+
+    public void ThrottleDown()
+    {
+        if (throttleIdeal >= 0.05f)
+            throttleIdeal -= 0.05f;
+
+        if (TaskList.Contains(Task.Throttle))
+            TaskList.Add(Task.Throttle);
+    }
+
+    public void SteerUp()
+    {
+        if (steerAngleIdeal <= 44.00f)
+            steerAngleIdeal += 1.00f;
+
+        if (TaskList.Contains(Task.Steer))
+            TaskList.Add(Task.Steer);
+    }
+
+    public void SteerDown()
+    {
+        if (steerAngleIdeal >= -44.00f)
+            steerAngleIdeal -= 1.00f;
+
+        if (TaskList.Contains(Task.Steer))
+            TaskList.Add(Task.Steer);
+    }
+
+    public void GoHooking()
+    {
+        hooking = !hooking;
+        if (TaskList.Contains(Task.Hook))
+            TaskList.Add(Task.Hook);
+    }
+
+    public void StokeYourself()
+    {
+        stoking = !stoking;
+        if (TaskList.Contains(Task.Stoke))
+            TaskList.Add(Task.Stoke);
+    }
+
+    
+	// Use this for initialization
+	void Start ()
+    {
+        TaskList = new List<Task>();
+    }
+    
+    /*
+	// Update is called once per frame
+	void Update ()
+    {
+		This will require access to the ship manager (Dance commander?) to pull the current values from
+	}
+    */
+}
