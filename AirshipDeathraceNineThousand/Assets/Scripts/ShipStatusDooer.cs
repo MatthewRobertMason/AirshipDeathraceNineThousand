aa1ac12b365e96e1c29769c9fd56f84a15e34cf1
matt <matt@matthewmason.ca>
2016-12-10 14:40:11 -0400

    commit pre merge

--- AirshipDeathraceNineThousand/Assets/Scripts/ShipStatusDooer.cs
+++ AirshipDeathraceNineThousand/Assets/Scripts/ShipStatusDooer.cs
@@ -7,12 +7,31 @@ public class ShipStatusDooer : MonoBehaviour
     public GameObject PeopleActionDecider;
     public GameObject steeringPointerActual;
     public GameObject throttleLevelActual;
+    public GameObject fuelLevel;
 
+    [SerializeField]
+    private float FUEL_USAGE = 0.05f;
+    [SerializeField]
+    private float THROTTLE_USAGE = 0.0005f;
+
+    [SerializeField]
+    private float STOKING_AMOUNT = 0.5f;
+    [SerializeField]
+    private float THROTTLE_AMOUNT = 0.05f;
+    
+    [Range(0, 1)]
+    [SerializeField]
     private float currentThrottle = 0.0f;
+    [Range(-45.0f, 45.0f)]
+    [SerializeField]
     private float currentSteeringAngle = 0.0f;
+
+    [SerializeField]
     private bool currentlyStoking = false;
+    [SerializeField]
     private bool currentlyHooking = false;
 
+    [Range(0.0f, 50.0f)]
     private float currentFuelLevel = 10.0f;
 
     // Use this for initialization
@@ -30,7 +49,25 @@ public class ShipStatusDooer : MonoBehaviour
         if (currentFuelLevel <= 0.0f)
             currentFuelLevel += 10.0f;
 
-        currentThrottle -= 0.0005f;
-        currentFuelLevel -= 0.05f;
+        currentThrottle -= THROTTLE_USAGE;
+        currentFuelLevel -= FUEL_USAGE;
+        
+        // Tick fuel usage
+        fuelLevel.transform.localScale = new Vector3(1.0f, (currentFuelLevel / 50.0f), 1.0f);
+
+        // Tick throttle usage
+        throttleLevelActual.transform.localScale = new Vector3(1.0f, currentThrottle, 1.0f);
+
+
+    }
+
+    public void toggleHook()
+    {
+        currentlyHooking = !currentlyHooking;
+    }
+
+    public void stokeFire()
+    {
+        currentFuelLevel += 0.2f;
     }
 }
