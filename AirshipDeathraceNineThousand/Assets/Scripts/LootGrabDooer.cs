99293591af5a3c380374c685ef938550bb9d53ca
Adam Douglass <douglass@malloc.ca>
2016-12-11 15:45:03 -0800

    Grab loot.

---
+++ AirshipDeathraceNineThousand/Assets/Scripts/LootGrabDooer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootGrabDooer : MonoBehaviour {
+
+	public GameObject hooked = null;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (hooked) {
+			PrizeMind prize = hooked.GetComponent<PrizeMind> ();
+			prize.transform.position = transform.position;
+		}		
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (hooked)
+			return;
+
+
+		hooked = other.gameObject;
+		hooked.GetComponent<PrizeMind>().hooked = true;
+	}
+}
