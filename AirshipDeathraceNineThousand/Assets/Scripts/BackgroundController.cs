d87aaecbb3dd36bba7817400d2274d4d2dcf798d
matt <matt@matthewmason.ca>
2016-12-10 22:04:43 -0400

    Added background, look at that ART!

--- AirshipDeathraceNineThousand/Assets/Scripts/BackgroundController.cs
+++ AirshipDeathraceNineThousand/Assets/Scripts/BackgroundController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class BackgroundController : MonoBehaviour {
 
+    // I'm not sure what the max should be, almost certainly not 500
+    [Range(0.0f, 1.0f)]
+    [SerializeField]
+    private float scrollspeed;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +16,12 @@ public class BackgroundController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        float y = Mathf.Repeat(Time.time * scrollspeed, 1);
+
+        Vector2 offset = new Vector2(y, 0);
+
+        this.GetComponent<Renderer>().material.SetTextureOffset("_MainTex", offset);
+
+        
 	}
 }
