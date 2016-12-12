a36a2e702b926998138bd899a85d6e64bc014fa1
matt <matt@matthewmason.ca>
2016-12-12 00:08:52 -0400

    Added more scenes, for menu and credits, created the crash scene but didn't add anything to it

---
+++ AirshipDeathraceNineThousand/Assets/Scripts/MenuButtonDooer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtonDooer : MonoBehaviour
+{
+    public void StartGame()
+    {
+        // load new game (airship.unity scene)
+        SceneManager.LoadScene(1);
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
